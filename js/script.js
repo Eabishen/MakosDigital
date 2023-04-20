058e46c3f501d2826459d9504b233013828da222
unknown <ebi4lbe@gmail.com>
2023-04-20 15:16:35 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,63 @@
+// window.addEventListener("resize", function() {
+//     "use strict"; window.location.reload(); 
+// });
+
+
+// document.addEventListener("DOMContentLoaded", function(){
+
+//     // make it as accordion for smaller screens
+//     if (window.innerWidth > 992) {
+
+//         document.querySelectorAll('.navbar .nav-item').forEach(function(everyitem){
+            
+//             everyitem.addEventListener('mouseover', function(e){
+
+//                 let el_link = this.querySelector('a[data-bs-toggle]');
+
+//                 if(el_link != null){
+//                     let nextEl = el_link.nextElementSibling;
+//                     el_link.classList.add('show');
+//                      nextEl.classList.add('show');
+//                 }
+                
+//             });
+//             everyitem.addEventListener('mouseleave', function(e){
+//                  let el_link = this.querySelector('a[data-bs-toggle]');
+                
+//                 if(el_link != null){
+//                     let nextEl = el_link.nextElementSibling;
+//                     el_link.classList.remove('show');
+//                      nextEl.classList.remove('show');
+//                 }
+                
+
+//             })
+//         });
+
+//     }
+//     // end if innerWidth
+// }); 
+// // DOMContentLoaded  end
+
+
+
+
+// const groupborder = document.querySelectorAll('.input-group');
+// const inputs = document.querySelectorAll('.form-control');
+
+// function changecolor(item) {
+
+//     console.log(item);
+//     if(item === document.activeElement){
+//         console.log("working")
+//         groupborder.forEach(group => {
+//             group.getElementsByClassName.borderColor = "#0078ff"
+//         })
+//     }
+
+// }
+
+
+// inputs.forEach(ele => {
+//     ele.addEventListener("click", changecolor(ele))
+// })
